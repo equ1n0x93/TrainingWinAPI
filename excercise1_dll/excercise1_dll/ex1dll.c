1bdcbb7281f6bac676b21b4c093beeb7c1e9b7c6
equinox <asaf.dahan.93@gmail.com>
2017-09-03 07:48:35 -0700

    ex1 DLL init

---
+++ excercise1_dll/excercise1_dll/ex1dll.c
@@ -0,0 +1,55 @@
+//ex1dll.c
+
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include "ex1dll.h"
+
+
+int createEchoProcess(createHidden)
+{
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	int createFlags = 0;
+	LPTSTR szCmdline = _tcsdup(TEXT("cmd /c echo \"hi\">ex1_dll.txt"));
+
+	if (createHidden) {
+		createFlags = CREATE_NO_WINDOW;
+	}
+
+	SecureZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+
+	SecureZeroMemory(&pi, sizeof(pi));
+
+	if (!CreateProcess(NULL,		           // Module name
+		szCmdline,                             // Commandline
+		NULL,						           // Process attributes
+		NULL,						           // Thread attributes
+		FALSE,						           // Does inherit handles from parent process
+		createFlags,		                   // Startup flags
+		NULL,						           // Environment block pointer
+		TEXT("C:\\Users\\User\\Desktop"),      // Process start CWD
+		&si,						           // StartupInfo Address(Pointer)
+		&pi)) 						           // ProcessInfo Address(Pointer)
+	{
+		printf("Process creation failed(error:%d).\n", GetLastError());
+		Sleep(3000);
+		return 1;
+	}
+
+	printf("Process created successfully\n");
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	return 0;
+}
+
+int createExcerciseProcess()
+{
+	int createProcessResultCode = createEchoProcess(FALSE);
+	return createProcessResultCode;
+}
---
+++ excercise1_dll/excercise1_dll/ex1dll.h
@@ -0,0 +1,8 @@
+// ex1dll.h - Contains declaration of Function class  
+#pragma once  
+
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+__declspec(dllexport) int createExcerciseProcess();
